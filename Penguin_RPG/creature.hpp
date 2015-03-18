e03fd7b03daeaece0615897330d10352e5b5a003
01aroe <adamroe89@gmail.com>
2015-03-18 15:08:34 +0000

    dialogue and creature functionality update
    updated the dialogue hpp features. Created creature hpp, implemented
    stats and levelling up

---
+++ Penguin_RPG/creature.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#ifndef CREATURE_HPP
+#define CREATURE_HPP
+
+#include <string>
+
+class Creature
+{
+public: 
+	Creature();
+	Creature(std::string name, int health, int str, int end, int dex, 
+		double hitRate, unsigned int level = 1, std::string className = "");
+
+	unsigned int expToLevel(unsigned int level);
+	bool levelUp();
+
+	//name of the creature and name of the class/vocation
+	std::string name;
+	std::string className;
+
+	//creature stats
+	int health;
+	int maxHealth;
+	int str;
+	int end;
+	int dex;
+	double hitRate;
+
+	//current level of creature
+	//determines the amount of exp given to the victor and 
+	//exp required the level up. After levelling up stats will
+	//increase. 0-50
+	unsigned int level;
+
+	//current exp. 0-1M
+	unsigned int exp;
+};
+
+Creature::Creature(std::string name, int health, int str, int end, int dex, 
+	double hitRate, unsigned int level = 1, std::string className = "")
+{
+	this->name = name;
+	this->health = health;
+	this->maxHealth = health;
+	this->str = str;
+	this->end = end;
+	this->dex = dex;
+	this->hitRate = hitRate;
+	this->className = className;
+	this->level = level;
+	this->exp = 0;
+}
+
+//calculates the exp required to reach a certain level
+//*in total*
+unsigned int Creature::expToLevel(unsigned int level)
+{
+	//exp to level x = 128*x^2
+	return 128 * (level * level);
+}
+
+//level the creature up if it has enough exp
+//returns true if enough exp, false if not
+bool Creature::levelUp()
+{
+	//we want the exp to the next level, not current level
+	if(this->exp >= expToLevel(level+1)
+	{
+		//level up
+		++level;
+
+		//vars to keep track of stat increases
+		unsigned int healthBoost = 0;
+		unsigned int strBoost = 0;
+		unsigned int endBoost = 0;
+		unsigned int dexBoost = 0;
+
+		//give a large boost to health every 3 levels
+		if(level % 3 == 0)
+		{
+			//randomly increase health, but keep proportional
+			//to creatures end
+			healthBoost = 10 + (rand() % 4) + (this->end / 4);
+		}
+		else
+		{
+			//standard health increase
+			healthBoost = (this->end / 4);
+		}
+
+		//if the creature is a fighter, favour str and end boosts
+		//Only increase dex 50% of the time
+		if(this->className == "Fighter")
+		{
+			strBoost = 1;
+			endBoost = 1;
+			if(rand() % 2 == 0)
+			{
+				dexBoost = 1;
+			}
+		}
+
+		//if the creature is a Rogue, favour dex and end boosts
+		//Only increase str 50% of the time
+		if(this->className == "Rogue")
+		{
+			dexBoost = 1;
+			endBoost = 1;
+			if(rand() % 2 == 0)
+			{
+				strBoost = 1;
+			}
+		}
+
+		//adjust all the variables based on increases
+		this->maxHealth += healthBoost;
+		this->str += strBoost;
+		this->end += endBoost;
+		this->dex += dexBoost;
+
+		//inform the user they grew a level, what boosts were and
+		//what the new stats for the creature are
+		std::cout << this->name << " grew to level" << level << "!" << std::endl;
+		std::cout << "Health +" << healthBoost << " ->"
+			<< this->maxHealth << std::endl;
+		std::cout << "Strength +" << strBoost << " ->"
+			<< this->str << std::endl;
+		std::cout << "Endurance +" << endBoost << " ->"
+			<< this->end << std::endl;
+		std::cout << "Dexterity +" << dexBoost << " ->"
+			<< this->dex << std::endl;
+		std::cout << "-----------------" << std::endl;
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+#endif //CREATURE_HPP
--- Penguin_RPG/dialogue.hpp
+++ Penguin_RPG/dialogue.hpp
@@ -1,5 +1,7 @@
-//#ifndef DIALOGUE_HPP
-//#define DIALOGUE_HPP
+#pragma once
+
+#ifndef DIALOGUE_HPP
+#define DIALOGUE_HPP
 
 #include <string>
 #include <vector>
@@ -12,18 +14,48 @@
 
 class Dialogue
 {
-public:
-	Dialogue();
-	Dialogue(std::string description, std::vector<std::string> choices)
-	{
-		this->description = description;
-		this->choices = choices;
-	}
-private:
 	//intial information to be displayed
 	std::string description;
 	//vector of choices
 	std::vector<std::string> choices;
+public:
+	Dialogue();
+	Dialogue(std::string description, std::vector<std::string> choices);
+	int activate();
 };
 
-//#endif //DIALOGUE_HPP
+Dialogue::Dialogue(std::string description, std::vector<std::string> choices)
+{
+	this->description = description;
+	this->choices = choices;
+}
+
+//run the dialogue
+int Dialogue::activate()
+{
+	//output the information
+	std::cout << description << std::endl;
+
+	//output and number the choices
+	for(int i = 0; i < this->choices.size(); i++)
+	{
+		std::cout << i+1 << ": " << this->choices[i] << std::endl;
+	}
+
+	int userInput = -1;
+
+	//repeatedly red input from stdin until a valid option is chosen
+	while(true)
+	{
+		std::cin >> userInput;
+
+		//valid is within the range of numbers outputted
+		if(userInput >= 0 && userInput <= this->choices.size())
+		{
+			return userInput;
+		}
+	}
+	return 0;
+}
+
+#endif //DIALOGUE_HPP
