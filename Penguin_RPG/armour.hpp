69523932c2874a236281cd7283e47721074a7fcf
01aroe <adamroe89@gmail.com>
2015-03-19 22:47:40 +0000

    Implemented weapon and armour classes
    Implemented the classes using the Item class as a base class.

--- Penguin_RPG/armour.hpp
+++ Penguin_RPG/armour.hpp
@@ -2,10 +2,31 @@
 #ifndef ARMOUR_HPP
 #define ARMOUR_HPP
 
-class Armour
+#include "item.hpp"
+#include <string>
+
+//armour should also inherit item properties
+class Armour: public Item
 {
 public:
+	//armour goes into 1 of 3 slots, only 1 piece can occupy
+	//each slot at a time. N is used to retrieve num of slots
+	enum Slot { TORSO, HEAD, LEGS, N };
+	Slot slot;
+
+	//battle class for more detail, values 1-50 reasonable
+	int defence;
+
 	Armour(){};
+	Armour(std::string name, std::string description, int defence, 
+		Armour::Slot slot) : Item(name, description)
 };
+
+Armour::Armour(std::string name, std::string description, int defence, 
+	Armour::Slot slot) : Item(name, description)
+{
+	this->defence = defence;
+	this->slot = slot;
+}
 
 #endif //ARMOUR_HPP
--- Penguin_RPG/weapon.hpp
+++ Penguin_RPG/weapon.hpp
@@ -11,6 +11,23 @@ class Weapon: public Item
 {
 public:
 	Weapon(){};
+
+	//weapon damage, battle class has formula
+	//values between 1-50 reasonable
+	unsigned int damage;
+
+	//hit rate modifier, small values encouraged 5-30%
+	double hitRate;
+
+	Weapon(std::string name, std::string description,
+		unsigned int damage, double hitRate) : Item(name, description);
 };
+
+Weapon::Weapon(std::string name, std::string description,
+	unsigned int damage, double hitRate) : Item(name, description)
+{
+	this->damage = damage;
+	this->hitRate = hitRate;
+}
 
 #endif //WEAPON_HPP
